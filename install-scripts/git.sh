93c0109a6c89ac675004160353f7528c44c19eb8
Iman Tabrizian <Tabrizian@outlook.com>
2016-07-31 00:30:25 +0430

    Minor bug fix

--- install-scripts/git.sh
+++ install-scripts/git.sh
@@ -1,3 +1,3 @@
 git config --global user.email "Tabrizian@outlook.com"
 git config --global user.name "Iman Tabrizian"
-git conifg --global push.default matching
+git config --global push.default matching
