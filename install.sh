50492b871754e1ef3dcac441d19857560248922a
Iman Tabrizian <Tabrizian@outlook.com>
2017-08-01 14:12:41 +0430

    Improves installation script

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@ ln -s ~/Documents/Git/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/Documents/Git/dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/Documents/Git/dotfiles/fonts ~/.fonts
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
